c2ded3d14b1600a28fdb67b548396ffc7d2f1807
Pratibhasagar V <pratibha@codeaurora.org>
2013-11-06 06:46:28 +0000

    uio: msm_sharedmem: Add new device driver for sharedmemory access
    Add new device driver to provide a standard interface for its
    clients (like RemoteFS and RFSA) to be able to memory map their
    respective allotted shared memory address in the client's address space.
    The shared memory (a transport buffer) address is unique for each
    individual client and is made available to the driver via device tree.
    
    This driver uses the existing UIO framework to facilitate the clients
    mmap requirements. Each individual client will be able to use a unique
    UIO device for this purpose.
    
    CRs-Fixed: 477427
    Change-Id: If07b88086b5f1b87845962818094644575629fcf
    Signed-off-by: Pratibhasagar V <pratibha@codeaurora.org>
    Signed-off-by: Venkat Gopalakrishnan <venkatg@codeaurora.org>
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>
    
    Conflicts:
    	drivers/uio/msm_sharedmem.c

--- drivers/uio/msm_sharedmem.c
+++ drivers/uio/msm_sharedmem.c
@@ -26,6 +26,14 @@ static int msm_sharedmem_probe(struct platform_device *pdev)
 	struct resource *clnt_res = NULL;
 
 	/* Get the addresses from platform-data */
+<<<<<<< HEAD
+=======
+	if (!pdev->dev.of_node) {
+		pr_err("Node not found\n");
+		ret = -ENODEV;
+		goto out;
+	}
+>>>>>>> 7f5fe61... uio: msm_sharedmem: Add new device driver for sharedmemory access
 	clnt_res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	if (!clnt_res) {
 		pr_err("resource not found\n");
