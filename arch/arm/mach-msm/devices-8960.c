bef6771eca757a1a4ead637546025fd88d662c2d
Jason Riordan <jriordan001@gmail.com>
2013-12-04 13:05:54 -0500

    GPU OC: add ifdef for gpu clock init levels. This fixes gpu fail when CONFIG_GPU_OVERCLOCK is not set

--- arch/arm/mach-msm/devices-8960.c
+++ arch/arm/mach-msm/devices-8960.c
@@ -3640,7 +3640,11 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.bus_freq = 0,
 		},
 	},
+#ifdef CONFIG_GPU_OVERCLOCK
 	.init_level = 3,
+#else
+	.init_level = 1,
+#endif
 	.num_levels = ARRAY_SIZE(grp3d_freq) + 1,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
@@ -3723,7 +3727,11 @@ static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 			.bus_freq = 0,
 		},
 	},
+#ifdef CONFIG_GPU_OVERCLOCK
 	.init_level = 3,
+#else
+	.init_level = 0,
+#endif
 	.num_levels = ARRAY_SIZE(grp2d_freq) + 1,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/5,
@@ -3806,7 +3814,11 @@ static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 			.bus_freq = 0,
 		},
 	},
+#ifdef CONFIG_GPU_OVERCLOCK
 	.init_level = 3,
+#else
+	.init_level = 0,
+#endif
 	.num_levels = ARRAY_SIZE(grp2d_freq) + 1,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/5,
