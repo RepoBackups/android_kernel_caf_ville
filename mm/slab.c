b79d20e37749d52acbcbc5c3224aeb3f88da32ed
rmbq <davide.rombola@gmail.com>
2013-12-05 18:39:04 +0100

    mm, sl[au]b: update from hammerhead kernel

--- mm/slab.c
+++ mm/slab.c
@@ -1685,15 +1685,15 @@ void __init kmem_cache_init_late(void)
 
 	g_cpucache_up = LATE;
 
+	/* Annotate slab for lockdep -- annotate the malloc caches */
+	init_lock_keys();
+
 	/* 6) resize the head arrays to their final sizes */
 	mutex_lock(&cache_chain_mutex);
 	list_for_each_entry(cachep, &cache_chain, next)
 		if (enable_cpucache(cachep, GFP_NOWAIT))
 			BUG();
 	mutex_unlock(&cache_chain_mutex);
-
-	/* Annotate slab for lockdep -- annotate the malloc caches */
-	init_lock_keys();
 
 	/* Done! */
 	g_cpucache_up = FULL;
@@ -4680,3 +4680,4 @@ size_t ksize(const void *objp)
 	return obj_size(virt_to_cache(objp));
 }
 EXPORT_SYMBOL(ksize);
+
