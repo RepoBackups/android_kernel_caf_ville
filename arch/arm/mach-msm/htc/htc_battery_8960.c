02a919dff50da0476d2d0737dcfc62cf346f24c4
Ethan Chen <intervigil@gmail.com>
2014-02-19 00:34:24 -0800

    msm: HTC: battery_8960: don't schedule update on suspend/resume
    * This is being scheduled on both early suspend and late resume. Don't
      do this here.
    
    Change-Id: I1ded619cb6f8d6e4f52682e4b6ffb048537151d2

--- arch/arm/mach-msm/htc/htc_battery_8960.c
+++ arch/arm/mach-msm/htc/htc_battery_8960.c
@@ -2303,8 +2303,6 @@ static void htc_battery_early_suspend(struct early_suspend *h)
 #ifdef CONFIG_HTC_BATT_ALARM
 	batt_set_voltage_alarm_mode(BATT_ALARM_DISABLE_MODE);
 #endif
-	htc_batt_schedule_batt_info_update();
-	return;
 }
 
 static void htc_battery_late_resume(struct early_suspend *h)
@@ -2314,7 +2312,6 @@ static void htc_battery_late_resume(struct early_suspend *h)
 #ifdef CONFIG_HTC_BATT_ALARM
 	batt_set_voltage_alarm_mode(BATT_ALARM_CRITICAL_MODE);
 #endif
-	htc_batt_schedule_batt_info_update();
 }
 #endif 
 
