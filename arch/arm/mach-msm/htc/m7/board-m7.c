535f4ca2e90c9f230d7302c3e74576f6ded3fb5b
Ethan Chen <intervigil@gmail.com>
2014-02-19 22:22:54 -0800

    msm: HTC: m7: Remove unnecessary usleep
    Change-Id: I1277e5315fb58c9b8f04c3ae72490cd4cbd0e08e

--- arch/arm/mach-msm/htc/m7/board-m7.c
+++ arch/arm/mach-msm/htc/m7/board-m7.c
@@ -1317,7 +1317,6 @@ static int synaptics_power_LPM(int on)
 			return rc;
 		}
 		pr_info("[TP] %s: leave LPM mode\n", __func__);
-		usleep(10);
 	}
 	mutex_unlock(&tp_lock);
 	return rc;
@@ -4244,7 +4243,6 @@ static int capella_pl_sensor_lpm_power(uint8_t enable)
 			return rc;
 		}
 		pr_info("[PS][cm3629] %s: leave lmp,OK\n", __func__);
-		usleep(10);
 	}
 	mutex_unlock(&pl_sensor_lock);
 	pr_info("[PS][cm3629] %s: pl_sensor_lock unlock 4\n", __func__);
