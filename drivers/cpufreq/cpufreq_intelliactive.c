369eccd439fe312ed6c6e1ba1c5024bbe976dabf
rmbq <davide.rombola@gmail.com>
2014-05-02 16:44:59 +0200

    fix build with Linaro GCC 4.9.1

--- drivers/cpufreq/cpufreq_intelliactive.c
+++ drivers/cpufreq/cpufreq_intelliactive.c
@@ -864,9 +864,9 @@ static ssize_t show_target_loads(
 		ret += sprintf(buf + ret, "%u%s", target_loads[i],
 			       i & 0x1 ? ":" : " ");
 
-	ret += sprintf(buf + --ret, "\n");
+	ret += sprintf(buf + (ret - 1), "\n");
 	spin_unlock_irqrestore(&target_loads_lock, flags);
-	return ret;
+	return ret - 1;
 }
 
 static ssize_t store_target_loads(
@@ -907,9 +907,9 @@ static ssize_t show_above_hispeed_delay(
 		ret += sprintf(buf + ret, "%u%s", above_hispeed_delay[i],
 			       i & 0x1 ? ":" : " ");
 
-	ret += sprintf(buf + --ret, "\n");
+	ret += sprintf(buf + (ret - 1), "\n");
 	spin_unlock_irqrestore(&above_hispeed_delay_lock, flags);
-	return ret;
+	return ret - 1;
 }
 
 static ssize_t store_above_hispeed_delay(
