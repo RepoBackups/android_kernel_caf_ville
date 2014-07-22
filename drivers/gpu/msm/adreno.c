51f626ed17f6691c7f38208e28958ffd5be4e66e
rmbq <davide.rombola@gmail.com>
2014-07-22 23:28:13 +0200

    drivers: gpu: msm: add config option to compile adreno 2xx and 3xx driver separately

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -188,6 +188,7 @@ static const struct {
 	   between CPU and GPU for SMMU-v1 programming */
 	unsigned int sync_lock_pfp_ver;
 } adreno_gpulist[] = {
+#ifdef CONFIG_MSM_ADRENO_2XX 
 	{ ADRENO_REV_A200, 0, 2, ANY_ID, ANY_ID,
 		"yamato_pm4.fw", "yamato_pfp.fw", &adreno_a2xx_gpudev,
 		512, 384, 3, SZ_256K, NO_VER, NO_VER },
@@ -213,6 +214,8 @@ static const struct {
 	{ ADRENO_REV_A225, 2, 2, ANY_ID, ANY_ID,
 		"a225_pm4.fw", "a225_pfp.fw", &adreno_a2xx_gpudev,
 		1536, 768, 3, SZ_512K, 0x225011, 0x225002 },
+#endif
+#ifdef CONFIG_MSM_ADRENO_3XX 
 	/* A3XX doesn't use the pix_shader_start */
 	{ ADRENO_REV_A305, 3, 0, 5, ANY_ID,
 		"a300_pm4.fw", "a300_pfp.fw", &adreno_a3xx_gpudev,
@@ -224,6 +227,7 @@ static const struct {
 	{ ADRENO_REV_A330, 3, 3, 0, ANY_ID,
 		"a330_pm4.fw", "a330_pfp.fw", &adreno_a3xx_gpudev,
 		512, 0, 2, SZ_1M, NO_VER, NO_VER },
+#endif
 };
 
 static unsigned int adreno_isidle(struct kgsl_device *device);
@@ -1664,6 +1668,7 @@ static int adreno_start(struct kgsl_device *device)
 	ft_detect_regs[0] = adreno_dev->gpudev->reg_rbbm_status;
 
 	/* Add A3XX specific registers for hang detection */
+#ifdef CONFIG_MSM_ADRENO_3XX 
 	if (adreno_is_a3xx(adreno_dev)) {
 		ft_detect_regs[6] = A3XX_RBBM_PERFCTR_SP_7_LO;
 		ft_detect_regs[7] = A3XX_RBBM_PERFCTR_SP_7_HI;
@@ -1672,7 +1677,7 @@ static int adreno_start(struct kgsl_device *device)
 		ft_detect_regs[10] = A3XX_RBBM_PERFCTR_SP_5_LO;
 		ft_detect_regs[11] = A3XX_RBBM_PERFCTR_SP_5_HI;
 	}
-
+#endif
 	status = kgsl_mmu_start(device);
 	if (status)
 		goto error_clk_off;
